af3d61b952b606b765a7c9fa1b8a627b344acdc1
jerryliang122 <jerryliang@jerryliang.top>
2023-05-26 20:39:55 +0800

    配置time out时间为5分钟

--- chatglm.py
+++ chatglm.py
@@ -13,7 +13,7 @@ class chatgpt():
                 "username": "kook",
                 "message": message
             }
-            async with session.post(str(backend_url), json=data) as response:
+            async with session.post(str(backend_url), json=data, timeout=300) as response:
                 response_text = await response.text()
                 try:
                     datas = json.loads(response_text)
